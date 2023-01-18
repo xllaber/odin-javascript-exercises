d63a91e98a5b2c251fd0a18f6b07a66aa7706c76
Ximo Llacer <xllaber7@gmail.com>
2023-01-18 09:17:53 +0100

    Do reverseString tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let answer = string;
+    answer = string.split("");
+    answer = answer.reverse();
+    answer = answer.join("");
+    return answer;
 };
 
 // Do not edit below this line
