d8985348680eaf4f9afb2b6926068d707a7e2dde
Ximo Llacer <xllaber7@gmail.com>
2023-02-03 08:47:38 +0100

    Do half

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(min, max) {
+    if (max < min) {
+        const aux = min;
+        min = max;
+        max = aux;
+    }
+    let result = 0;
+    for (let i = min; i <= max; i++) {
+        result+=i;
+    }
+    return result;
 };
 
 // Do not edit below this line
